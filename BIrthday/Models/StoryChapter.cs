4dd1456e3780f57f8169f35cd3005bc658176f17
Krishna <krishnapdbashyal23@gmail.com>
2026-01-28 15:53:19 -0500

    feat: extend story chapters with mood and final markers

--- BIrthday/Models/StoryChapter.cs
+++ BIrthday/Models/StoryChapter.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
+
+        public Boolean isFinal { get; set; }
+        public string Mood { get; set; }
     }
 }
 // learn to insert emojis {get, set}="";
