7aef1c8b95d5e3c3d153d907569596c457bd38d1
Krishna <krishnapdbashyal23@gmail.com>
2026-01-29 01:10:10 -0500

    feat: refine chapters writing and pacing

--- BIrthday/Controllers/StoryController.cs
+++ BIrthday/Controllers/StoryController.cs
@@ -13,17 +13,33 @@ namespace BIrthday.Controllers
                 {
                     Id=0,
                     Title="yooo 🫡",
-                    Body= "This isn't just a webiste.It's a small pause. "
+                    Mood="calm",
+                    Body= "This isn't just a webiste.\nIt's a small pause.\n A quiet moment before i reveal something dumb "
                 },
                 new StoryChapter
                 {
                     Id=1,
                     Title="A quiet chapter",
+                    Mood="reflective",
                     Body="For someone who moves through the world with kindness, curiosity and aspires to a pishachini"
                 },
                 new StoryChapter
                 {
                     Id=2,
+                    Title="Some people carry stories gently",
+                    Mood="reflective",
+                    Body="They dont announce themselves loudly, but you /n you yap a lot"
+                },
+                 new StoryChapter
+                 {
+                     Id=3,
+                     Title="You once gave me a book",
+                     Mood="warm",
+                     Body="A story about finding light where none should exist "
+                 },
+                new StoryChapter
+                {
+                    Id=3,
                     Title="And today...",
                     Body=" This chapter is yours."
                 }
